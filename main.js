f3b28c1f3ead6048e63cc39ce449d1cc3cf598e5
madan-ferreira <madan_fm@hotmail.com>
2023-04-04 17:36:22 -0300

    General order indicators added

--- main.js
+++ main.js
@@ -161,44 +161,131 @@ function update(){
      } 
      click = true;
 }
-function monthProg(ordersInd){
-     ordersInd = document.createElement("tr");
-     ordersInd.id = "ordersInd";
-     ordersInd.classList.add("ordersInd");
-     document.getElementById("projections").appendChild(ordersInd);
+function monthProg(monthProg, titleProgTr){
+     titleProgTr = document.createElement("tr");
+     titleProgTr.id = "titleProgTr";
+     titleProgTr.classList.add("titleTr");
+     document.getElementById("projections").appendChild(titleProgTr);
+     let titleProg = document.createElement("th");
+     titleProg.id = "title-prog";
+     titleProg.classList.add("titleTh");
+     document.getElementById("titleProgTr").appendChild(titleProg);
+     document.getElementById("title-prog").colSpan = "25";
+     document.getElementById("title-prog").innerHTML = "Indicadores gerais para pedidos";
+
+     monthProg = document.createElement("tr");
+     monthProg.id = "monthProg";
+     monthProg.classList.add("monthProg");
+     document.getElementById("projections").appendChild(monthProg);
+     let titleMonth = document.createElement("td");
+     titleMonth.id = "title-month";
+     titleMonth.classList.add("title");
+     document.getElementById("monthProg").appendChild(titleMonth);
+     document.getElementById("title-month").innerHTML = "Meses";
 
      for (let m = 1; m < 25; m++) {
           let month = document.createElement("td");
-          month.id = "orders-ind-" + m;
-          month.classList.add("month");
+          month.id = "month-prog" + m;
+          month.classList.add("progression");
           month.innerText = m;
-          document.getElementById("ordersInd").appendChild(month);
+          document.getElementById("monthProg").appendChild(month);
      }
-     tableProj();
+     projMonth();
 }
 
-function tableProj(endNumber, numberPerDay, numberProg, perMonth) {
+function projMonth(endNumber, numberPerDay, numberProg, orderMonth, costAllOrders, comission, comissionProj) {
           endNumber = (getPop() * (getOrderPercentage() / 100)) / 30;
+          comission = getOrderCommission() / 100;
+
           let endNumberInt = parseInt(endNumber);
           numberPerDay = document.createElement("tr");
           numberPerDay.id = "numberPerDay";
           numberPerDay.classList.add("numberPerDay");
           document.getElementById("projections").appendChild(numberPerDay);
+          let titleEnd = document.createElement("td");
+          titleEnd.id = "title-end";
+          titleEnd.classList.add("title");
+          document.getElementById("numberPerDay").appendChild(titleEnd);
+          document.getElementById("title-end").innerHTML = "Número de pedidos por dia";
 
-          for (let i = 1; i < 25; i++) {
+          orderMonth = document.createElement("tr");
+          orderMonth.id = "orderMonth";
+          orderMonth.classList.add("orderMonth");
+          document.getElementById("projections").appendChild(orderMonth);
+          let titleOrderM = document.createElement("td")
+          titleOrderM.id = "titleOrderM";
+          titleOrderM.classList.add("title");
+          document.getElementById("orderMonth").appendChild(titleOrderM);
+          document.getElementById("titleOrderM").innerHTML = "Número de pedidos por mês";
+
+
+          costAllOrders = document.createElement("tr");
+          costAllOrders.id = "costAllOrders";
+          costAllOrders.classList.add("costAllOrders");
+          document.getElementById("projections").appendChild(costAllOrders);
+          let titleCost = document.createElement("td")
+          titleCost.id = "title-cost";
+          titleCost.classList.add("title");
+          document.getElementById("costAllOrders").appendChild(titleCost);
+          document.getElementById("title-cost").innerHTML = "Custo de todas as viagens";
+
+          comissionProj = document.createElement("tr");
+          comissionProj.id = "comissionProj";
+          comissionProj.classList.add("comissionProj");
+          document.getElementById("projections").appendChild(comissionProj);
+          let titleComission = document.createElement("td")
+          titleComission.id = "title-comission";
+          titleComission.classList.add("title");
+          document.getElementById("comissionProj").appendChild(titleComission);
+          document.getElementById("title-comission").innerHTML = "Comissão para todos os pedidos";
+
+          let costMonth = document.createElement("tr");
+          costMonth.id = "cost-month";
+          costMonth.classList.add("costMonth");
+          document.getElementById("projections").appendChild(costMonth);
+          let titleCostM = document.createElement("th")
+          titleCostM.id = "title-costM";
+          titleCostM.classList.add("titleTh");
+          document.getElementById("cost-month").appendChild(titleCostM);
+          document.getElementById("title-costM").colSpan = "25";
+          document.getElementById("title-costM").innerHTML = "Todas as despesas";
+
+          for (let i = 1, j = 0; i < 25; i++) {
                let number = document.createElement("td");
                number.id = "number-per-day-" + i;
-               number.classList.add("number-per-day");
+               number.classList.add("progression");
                document.getElementById("numberPerDay").appendChild(number);
+
           }
 
           document.getElementById("number-per-day-24").innerHTML = endNumberInt;
 
-          for (let n = 1; n < 24; n++) {
+          for (let n = 1, f = 1; n < 24, f < 25; n++, f++) {
                numberProg = (endNumber / 24) * n;
                let numberProgInt = parseInt(numberProg);
                document.getElementById("number-per-day-" + n).innerHTML = numberProgInt;
+
+               let calcOrderM = (numberProgInt * 30);
+               let ordersM = document.createElement("td");
+               ordersM.id = "order-month-" + f;
+               ordersM.classList.add("progression");
+               document.getElementById("orderMonth").appendChild(ordersM);
+               document.getElementById("order-month-" + f).innerHTML = calcOrderM.toFixed(0);
+
+               let calcCostAll = calcOrderM * getAveragePrice()
+               let costAll = document.createElement("td");
+               costAll.id = "cost-all-" + f;
+               costAll.classList.add("progression");
+               document.getElementById("costAllOrders").appendChild(costAll);
+               document.getElementById("cost-all-" + f).innerHTML = calcCostAll.toFixed(0);
+
+               let calcComission = calcCostAll * comission;
+               let comissionProj = document.createElement("td");
+               comissionProj.id = "comission-" + f;
+               comissionProj.classList.add("progression");
+               document.getElementById("comissionProj").appendChild(comissionProj);
+               document.getElementById("comission-" + f).innerHTML = calcComission.toFixed(0);
           }
-          return;
+          projCosts();
      }
 
